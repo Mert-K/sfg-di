4b76fdd9745a18a186d977b2cb318672391ff96e
Mert Kurt <tyrael91@hotmail.com>
2024-05-22 17:37:57 +0300

    Total Commit

---
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgdi.services;
+
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/27/19.
+ */
+
+public class I18nEnglishGreetingService implements GreetingService {
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
+    @Override
+    public String sayGreeting() {
+        return englishGreetingRepository.getGreeting();
+    }
+}
